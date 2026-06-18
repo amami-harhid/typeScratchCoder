20b00546c1afd066083e96bc0fb2da072b8a1af8
Amami Harhid <128221872+amami-harhid@users.noreply.github.com>
2026-06-18 04:28:23 +0000

    20260618 #03

--- src/02_practices/001/index.ts
+++ src/02_practices/001/index.ts
@@ -8,13 +8,14 @@ import type { Sprite } from "@tscratch3/typescratcher";
 
 // 【画像 import 】
 import CatSvg from "@Assets/cat.svg";
+import Cat2Svg from "@Assets/cat2.svg";
 import BasketballPng from "@Assets/Basketball 2.png";
 
 // 【音 import】
 import CatWav from "@Assets/Cat.wav";
-
 // イメージ作成
 const CatImage = new Ts.Image({ CatSvg });
+const Cat2Image = new Ts.Image({ Cat2Svg });
 const BasketballImage = new Ts.Image({ BasketballPng });
 
 // サウンド作成
@@ -22,7 +23,7 @@ const CatSound = new Ts.Sound({ CatWav });
 
 // スプライト作成
 const cat = new Ts.Sprite("apple");
-cat.Costume.add([CatImage]); // イメージを１個追加
+cat.Costume.add([CatImage, Cat2Image]); // イメージを追加
 cat.Sound.add([CatSound]); // サウンドを１個追加
 
 // ステージ作成
@@ -31,35 +32,45 @@ stage.Backdrop.add([BasketballImage]); // 背景を１個追加
 
 // 旗を押したときのイベント定義
 cat.Event.flagPresser().func = async function* (this: Sprite) {
-  // 回転を「回転しない」にする
-  this.Motion.rotation.style = Ts.Rotation.DONT_ROTATE;
-  // 角度を設定する
-  this.Motion.direction.degree = 90;
-  // 位置を中央にする
-  this.Motion.position.xy = [0, 0];
-  // 大きさを大きくする( 横、縦のパーセンテージ )
-  this.Looks.size.scale = [200, 200];
+    // 回転を「左右のみ」にする
+    this.Motion.rotation.style = Ts.Rotation.LEFT_RIGHT;
+    // 角度を設定する
+    this.Motion.direction.degree = 90;
+    // 位置を中央にする
+    this.Motion.position.xy = [0, -120];
+    // 大きさを大きくする( 横、縦のパーセンテージ )
+    this.Looks.size.scale = [100, 100];
 };
 
 // 旗を押したときのイベント定義
 cat.Event.flagPresser().func = async function* (this: Sprite) {
-  // ずっと繰り返す
-  for (;;) {
-    // クリックしたとき
-    if (this.Sensing.mouse.isTouching && this.Sensing.mouse.isDown) {
-      console.log("isClicked");
-      this.Sound.play(CatSound);
-      this.Control.clone();
+    // ずっと繰り返す
+    for (;;) {
+        if (this.Sensing.keyboard.isDown(Ts.Keyboard.SPACE)) {
+            // ジャンプ・等速
+            for (const _ of Ts.Loop.Iterator(10)) {
+                // 上昇
+                this.Motion.position.y += 5;
+                yield;
+            }
+            for (const _ of Ts.Loop.Iterator(10)) {
+                // 下降
+                this.Motion.position.y -= 5;
+                yield;
+            }
+        } else if (this.Sensing.keyboard.isDown(Ts.Keyboard.RIGHT)) {
+            // 右を向く
+            this.Motion.direction.degree = 90;
+            this.Motion.move.steps(5);
+            this.Looks.costume.next();
+        } else if (this.Sensing.keyboard.isDown(Ts.Keyboard.LEFT)) {
+            // 左を向く
+            this.Motion.direction.degree = -90;
+            this.Motion.move.steps(5);
+            this.Looks.costume.next();
+        }
+        yield;
     }
-    yield;
-  }
-};
-// クローンされたとき
-cat.Event.cloned().func = async function* (this: Sprite) {
-  this.Looks.size.scale = [20, 20];
-  for (;;) {
-    yield;
-  }
 };
 
 // 開始
