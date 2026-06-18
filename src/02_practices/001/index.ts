bdd7b8c9e80996bb481f79747cd983bd1d45b885
Amami Harhid <128221872+amami-harhid@users.noreply.github.com>
2026-06-18 01:47:30 +0000

    20260618 #02

--- src/02_practices/001/index.ts
+++ src/02_practices/001/index.ts
@@ -1,68 +1,66 @@
 /**
  * 練習-001
- * スプライトをクリックしたらクローンを作る
- * クローンされたとき、ランダムな方向へ動き出す
+ * スペースキーで等速ジャンプをする
+ * 左右矢印キーで左右に歩く
  */
-import { Typescratcher as Ts } from '@tscratch3/typescratcher';
-import type { Sprite } from '@tscratch3/typescratcher';
+import { Typescratcher as Ts } from "@tscratch3/typescratcher";
+import type { Sprite } from "@tscratch3/typescratcher";
 
 // 【画像 import 】
-import CatSvg from '@Assets/cat.svg';
-import BasketballPng from '@Assets/Basketball 2.png';
+import CatSvg from "@Assets/cat.svg";
+import BasketballPng from "@Assets/Basketball 2.png";
 
 // 【音 import】
-import CatWav from '@Assets/Cat.wav';
+import CatWav from "@Assets/Cat.wav";
 
 // イメージ作成
-const CatImage = new Ts.Image({CatSvg});
-const BasketballImage = new Ts.Image({BasketballPng});
+const CatImage = new Ts.Image({ CatSvg });
+const BasketballImage = new Ts.Image({ BasketballPng });
 
 // サウンド作成
-const CatSound = new Ts.Sound({CatWav});
+const CatSound = new Ts.Sound({ CatWav });
 
 // スプライト作成
-const cat = new Ts.Sprite('apple');
-cat.Costume.add( [CatImage] ); // イメージを１個追加
-cat.Sound.add( [CatSound] ); // サウンドを１個追加
+const cat = new Ts.Sprite("apple");
+cat.Costume.add([CatImage]); // イメージを１個追加
+cat.Sound.add([CatSound]); // サウンドを１個追加
 
 // ステージ作成
 const stage = new Ts.Stage();
-stage.Backdrop.add( [BasketballImage] ); // 背景を１個追加
+stage.Backdrop.add([BasketballImage]); // 背景を１個追加
 
 // 旗を押したときのイベント定義
-cat.Event.flagPresser().func = async function*(this:Sprite){
-    // 回転を「回転しない」にする
-    this.Motion.rotation.style = Ts.Rotation.DONT_ROTATE;
-    // 角度を設定する
-    this.Motion.direction.degree = 90;
-    // 位置を中央にする
-    this.Motion.position.xy = [0, 0]; 
-    // 大きさを大きくする( 横、縦のパーセンテージ )
-    this.Looks.size.scale = [200, 200];
-}
+cat.Event.flagPresser().func = async function* (this: Sprite) {
+  // 回転を「回転しない」にする
+  this.Motion.rotation.style = Ts.Rotation.DONT_ROTATE;
+  // 角度を設定する
+  this.Motion.direction.degree = 90;
+  // 位置を中央にする
+  this.Motion.position.xy = [0, 0];
+  // 大きさを大きくする( 横、縦のパーセンテージ )
+  this.Looks.size.scale = [200, 200];
+};
 
 // 旗を押したときのイベント定義
-cat.Event.flagPresser().func = async function*(this:Sprite){
-    // ずっと繰り返す
-    for(;;){
-        // クリックしたとき
-        if(this.Sensing.mouse.isTouching 
-                && this.Sensing.mouse.isDown) {
-            console.log('isClicked')
-            this.Sound.play(CatSound);
-            this.Control.clone();
-        }
-        yield;
+cat.Event.flagPresser().func = async function* (this: Sprite) {
+  // ずっと繰り返す
+  for (;;) {
+    // クリックしたとき
+    if (this.Sensing.mouse.isTouching && this.Sensing.mouse.isDown) {
+      console.log("isClicked");
+      this.Sound.play(CatSound);
+      this.Control.clone();
     }
-}
+    yield;
+  }
+};
 // クローンされたとき
-cat.Event.cloned().func = async function*(this:Sprite) {
-    this.Looks.size.scale = [20, 20];
-    for(;;) {
-        
-        yield;
-    }
-}
+cat.Event.cloned().func = async function* (this: Sprite) {
+  this.Looks.size.scale = [20, 20];
+  for (;;) {
+    yield;
+  }
+};
 
 // 開始
 Ts.engine.start();
