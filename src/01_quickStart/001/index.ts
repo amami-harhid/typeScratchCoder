d7330fc5fb4e15c11be0818442b2348d36938c58
amami-harhid <amami-thida100@outlook.jp>
2026-08-27 21:36:43 +0900

    moduleResolution bundler

--- src/01_quickStart/001/index.ts
+++ src/01_quickStart/001/index.ts
@@ -2,6 +2,6 @@
  * サンプル-001
  * 最初の一歩：起動
  */
-import { Typescratcher as Ts } from "@tscratch3/typescratcher";
+import { Typescratcher as Ts } from "@tscratch3/typescratcher/index";
 // 開始
 Ts.engine.start();
